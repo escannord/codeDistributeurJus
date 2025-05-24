d435ea4d8acde4b57ff0e10eb6c88500f96a2fc8
Arnold NGUECHOUNG <nguechoungarnold@gmail.com>
2025-05-24 18:55:20 +0000

    ajustement des parametres

--- codeDistributeurJus.ino
+++ codeDistributeurJus.ino
@@ -195,7 +195,7 @@ void desactiverToutesPompes() {
   if (isPumpActive) {  // Si la pompe était active
     timeToService = (millis() - startTime) / 1000.0;  // Ajoute le temps en secondes
     isPumpActive = false;
-    volumeTimer = timeToService * 450 / 245;
+    volumeTimer = timeToService * 430 / 215;
     sendToAPI(selectedPump, volumeTimer, "completed");
   }
   digitalWrite(RELAY_PUMP1, LOW);
